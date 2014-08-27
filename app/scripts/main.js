58002ef1bc77339b0e927724e41353b4beaeff9d
Bob <jiafeifei@doozii.com>
2014-08-27 13:24:51 +0800

    Bob: custom arrow & dots

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,11 @@
 $(function() {
 	$("#bob").fullpage({
 				css3: true,
-				slidesColor: ['#19AEEE', '#19AEEE', '#19AEEE', '#19AEEE', '#19AEEE'],
+				// slidesColor: ['#19AEEE', '#19AEEE', '#19AEEE', '#19AEEE', '#19AEEE'],
 				anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
 				loopBottom: true,
-				slidesNavigation: true
+				slidesNavigation: true,
+				slidesNavPosition: "top"
 
 
 			});
