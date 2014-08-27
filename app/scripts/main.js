2d1b36230395085325112bca16537d6ec0b30425
Bob <jiafeifei@doozii.com>
2014-08-27 09:40:00 +0800

    Bob:first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+$(function() {
+	$("#bob").fullpage({
+				css3: true,
+				slidesColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+				anchors: ['page1', 'page2', 'page3', 'page4'],
+				loopBottom: true,
+				slidesNavigation: true
+
+
+			});
+});
