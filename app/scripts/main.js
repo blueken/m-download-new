3f90985cc02fff28043829d17a628eb5ee14b20e
Bob <jiafeifei@doozii.com>
2014-08-28 16:11:02 +0800

    Bob: 1. fix str.match(//i) === string

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -50,7 +50,7 @@ function callfullpage() {
 function isWeiXin() {
 	'use strict';
     var ua = window.navigator.userAgent.toLowerCase();
-    if(ua.match(/MicroMessenger/i) === 'micromessenger'){
+    if(ua.match(/MicroMessenger/i) == 'micromessenger'){
         return true;
     }else{
         return false;
